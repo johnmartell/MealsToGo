8d551c41ffeacc67aed46cdb1d0ae7bf45519520
johnmartell <john@tallhammer.com>
2021-08-20 22:01:33 -0700

    added views, statusbars

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, View, SafeAreaView, Platform, ScrollView } from 'react-native';
+
+console.log(StatusBar.currentHeight);
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "white", alignItems: "center", justifyContent: "center"}}>
-      <Text>Our react native blank canvas</Text>
-    </View>
+    <>
+      <SafeAreaView style={styles.SafeAreaView}>
+        <View style={{padding: 16, backgroundColor:"green"}}>
+          <Text>Search</Text>
+        </View>
+        <ScrollView style={{flex: 1, padding: 16, backgroundColor: "blue"}}>
+          <Text>List goes here</Text>
+        </ScrollView>
+      </SafeAreaView>
+      <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  SafeAreaView: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight
+  },
 });
